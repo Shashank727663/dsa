615510bd27cae17a523df0a06c271282d86f06fd
shashank krishu <78855878+Shashank727663@users.noreply.github.com>
2023-07-01 21:53:38 +0530

    Added solution - LeetHub

---
+++ Possible paths between 2 vertices - GFG/possible-paths-between-2-vertices.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+    private:
+    
+    void dfs(int start ,int end , vector<int>adj[] , int &ans ) {
+        if(start == end) {
+            ans++;
+            return;
+        }
+        
+        
+        for(auto x : adj[start]) {
+            dfs(x,end,adj,ans);
+        }
+        
+        return;
+     }
+  public:
+    // Function to count paths between two vertices in a directed graph.
+    int countPaths(int V, vector<int> adj[], int source, int destination) {
+        // Code here
+        
+        
+        int ans = 0;
+        dfs(source,destination,adj,ans);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // adj[v].push_back(u);
+        }
+        int source, destination;
+        cin >> source >> destination;
+        Solution obj;
+        cout << obj.countPaths(V, adj, source, destination) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
